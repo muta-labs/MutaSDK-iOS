38e88156f14d89422184b79676f156e9a13d008b
fostercalls <mwfoster44@gmail.com>
2025-07-25 13:05:52 -0700

    Fix Package.swift formatting

--- Package.swift
+++ Package.swift
@@ -15,9 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MutaSDK",
-            url: "https://firebasestorage.googleapis.com/v0/b/muta-ae90b.firebasestorage.app/o/relea
-  ses%2Fv1.0.1%2FMutaSDK.xcframework.zip?alt=media&token=a1b12592-a580-4c7d-bc1a-957b9e783a7d",
+            url: "https://firebasestorage.googleapis.com/v0/b/muta-ae90b.firebasestorage.app/o/releases%2Fv1.0.1%2FMutaSDK.xcframework.zip?alt=media&token=a1b12592-a580-4c7d-bc1a-957b9e783a7d",
             checksum: "7ead33a7f43db8ece4652ef789958e561af06fcd464350c0438d00819f3fcc0d"
         )
     ]
-) 
+)
