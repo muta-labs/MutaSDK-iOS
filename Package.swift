dbc2c57468c0b88643d6526122eb69bb553885a7
fostercalls <mwfoster44@gmail.com>
2025-07-25 12:46:25 -0700

    Release v1.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MutaSDK",
-            url: "https://firebasestorage.googleapis.com/v0/b/muta-ae90b.firebasestorage.app/o/releases%2Fv1.0.0%2FMutaSDK.xcframework.zip?alt=media&token=ad1dd8ff-043e-43fc-a7b6-7ca8d14e43b0",
-            checksum: "38d1c2ca6fa4958eacf42787e6dfcdf4476e7c326a23eb600211693f4d7fe5be"
+            url: "https://firebasestorage.googleapis.com/v0/b/muta-ae90b.firebasestorage.app/o/relea
+  ses%2Fv1.0.1%2FMutaSDK.xcframework.zip?alt=media&token=a1b12592-a580-4c7d-bc1a-957b9e783a7d",
+            checksum: "7ead33a7f43db8ece4652ef789958e561af06fcd464350c0438d00819f3fcc0d"
         )
     ]
 ) 
